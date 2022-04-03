56e3fec9e1a2dd8ce041f7fcd2b5b503d6cff181
Jorge Mena <jormencar@gmail.com>
2022-04-03 16:58:25 +0100

    initial commit

---
+++ js/hand-challenge/test.js
@@ -0,0 +1,18 @@
+/* global test, expect */
+const translate = require('./index.js')
+
+test('it traslate hello', () => {
+  expect(
+    translate(
+      '👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊'
+    )
+  ).toBe('Hello')
+})
+
+test('it traslate hello world', () => {
+  expect(
+    translate(
+      '👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊'
+    )
+  ).toBe('Hello World!\n')
+})
