3151df940fc86ef606cfa5ce7ddea0be639c40be
aljorgevi <jormencar@yahoo.com>
2022-04-16 00:00:46 +0100

    test

--- Java/src/main/java/exercices/FindEvenNumberTwoArrays.java
+++ Java/src/main/java/exercices/FindEvenNumberTwoArrays.java
@@ -6,27 +6,26 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /*
-*   Find the even numbers in two arrays and return a new array.
-* */
-
+ *   Find the even numbers in two arrays and return a new array.
+ * */
 
 public class FindEvenNumberTwoArrays {
-    public static int[] findEvenNum(int[] arr1, int[] arr2) {
-        IntPredicate predicate = x -> x % 2 == 0;
-        return Stream.of(arr1, arr2)
-                .flatMapToInt(Arrays::stream)
-                .filter(predicate)
-                .toArray();
-    }
 
-    public static void main(String[] args) {
-        int[] arr1 = { -9, 3, 2, -8, 12, -16 };
-        int[] arr2 = { 0, -3, -8, -35, 40, 20, 7 };
+  public static int[] findEvenNum(int[] arr1, int[] arr2) {
+    IntPredicate intPredicate = x -> x % 2 == 0;
 
-//        Arrays.stream(findEvenNum(arr1, arr2))
-//                .forEach(System.out::println);
-        int[] evenNum = findEvenNum(arr1, arr2);
-        System.out.println(Arrays.toString(evenNum));
+    return Stream
+      .of(arr1, arr2)
+      .flatMapToInt(Arrays::stream)
+      .filter(intPredicate)
+      .toArray();
+  }
 
-    }
+  public static void main(String[] args) {
+    int[] arr1 = { -9, 3, 2, -8, 12, -16 };
+    int[] arr2 = { 0, -3, -8, -35, 40, 20, 7 };
+
+    int[] result = findEvenNum(arr1, arr2);
+    System.out.println(Arrays.toString(result));
+  }
 }
