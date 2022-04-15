37932e113f02d80247407bb8feda62a9996d2b74
aljorgevi <jormencar@yahoo.com>
2022-04-15 23:37:16 +0100

    re-format

--- Java/src/main/java/exercices/EvenOrOdd.java
+++ Java/src/main/java/exercices/EvenOrOdd.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class EvenOrOdd {
 
-    public static boolean isEven(int n) {
-        return n % 2 == 0;
-    }
+  public static boolean isEven(int n) {
+    return n % 2 == 0;
+  }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
-        int userNum = scanner.nextInt();
-        System.out.println("Is the number even? " + isEven(userNum));
-    }
+    System.out.println("Enter a number: ");
+    int userNum = scanner.nextInt();
+    System.out.println("Is the number even? " + isEven(userNum));
+  }
 }
