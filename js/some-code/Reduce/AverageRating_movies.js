95edb71c0230d7487f2445f1b9d42a4d6e33c832
aljorgevi <jormencar@yahoo.com>
2022-04-23 21:38:40 +0100

    add reduce examples & change definition

--- js/some-code/Reduce/AverageRating_movies.js
+++ js/some-code/Reduce/AverageRating_movies.js
@@ -1,24 +1,4 @@
 /*
-  Array.prototype.reduce(), or simply reduce(), is the most general of all array operations in JavaScript.
-  You can solve almost any array processing problem using the reduce method.
-
-  The reduce method allows for more general forms of array processing,
-  and it's possible to show that both filter and map can be derived as special applications of reduce.
-  The reduce method iterates over each item in an array and returns a single value
-  (i.e. string, number, object, array). This is achieved via a callback function that is called on each iteration.
-
-  The callback function accepts four arguments.
-  The first argument is known as the accumulator, which gets assigned the return value of the callback function from the previous iteration.
-  The second is the current element being processed.
-  The third is the index of that element and the fourth is the array upon which reduce is called.
-
-  In addition to the callback function, reduce has an additional parameter which takes an initial value for the accumulator.
-  If this second parameter is not used, then the first iteration is skipped,
-  and the second iteration gets passed the first element of the array as the accumulator.
-
-  See below for an example using reduce on the users array to return the sum of all the users' ages.
-  For simplicity, the example only uses the first and second arguments.
-
   The variable watchList holds an array of objects with information on several movies.
   Use reduce to find the average IMDB rating of the movies directed by Christopher Nolan.
   Recall from prior challenges how to filter data and map over it to pull what you need. You may need to create other variables,
@@ -146,8 +126,6 @@ const watchList = [
     Response: 'True'
   }
 ];
-
-
 
 function getRating(watchList) {
  const nolanData = watchList.reduce((acc, el) => {
