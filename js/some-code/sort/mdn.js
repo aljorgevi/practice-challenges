c0ba2e00f683f63395729e4d30b95f285d467a95
aljorgevi <jormencar@yahoo.com>
2022-04-24 10:52:32 +0100

    add sort

--- js/some-code/sort/mdn.js
+++ js/some-code/sort/mdn.js
@@ -62,4 +62,17 @@ numericStringArray.sort(compareNumbers); // ['9', '80', '700']
 mixedNumericArray.sort(); // [1, 200, 40, 5, '700', '80', '9']
 mixedNumericArray.sort(compareNumbers); // [1, 5, '9', 40, '80', 200, '700']
 
+/*
+  Sorting non-ASCII characters
+  For sorting strings with non-ASCII characters, i.e. strings with accented characters (e, é, è, a, ä, etc.), strings from languages other than English, use String.localeCompare. This function can compare those characters so they appear in the right order.
+*/
+{
+  const items = ['réservé', 'premier', 'communiqué', 'café', 'adieu', 'éclair']
+  items.sort(function (a, b) {
+    return a.localeCompare(b)
+  })
+
+  // items is ['adieu', 'café', 'communiqué', 'éclair', 'premier', 'réservé']
+}
+
 
